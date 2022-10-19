c46732b67024816b0d846ac90716a8dd6f9b3b8e
Charles Paperman <charles.paperman@univ-lille.fr>
2022-10-19 18:44:46 +0200

    crossref with more datasets and scalability (requires a lot of RAM)

--- benches/crossref.rs
+++ benches/crossref.rs
@@ -10,7 +10,6 @@ use rsonpath_benchmarks::rust_jsonski;
 use std::fs;
 
 const ROOT_TEST_DIRECTORY: &str = "./data";
-const CROSSREF_DATA_PATH : &str = "crossref/crossref.json";
 fn get_jsonski_record(test_path: &str) -> rust_jsonski::JsonSkiRecord {
     let path = format!("{}/{}", ROOT_TEST_DIRECTORY, test_path);
     rust_jsonski::load_jsonski_record(&path)
@@ -32,17 +31,9 @@ struct BenchmarkOptions<'a> {
     pub measurement_time: Duration,
 }
 
-fn crossref(c: &mut Criterion, options: BenchmarkOptions<'_>) {
-    let context = rust_jsurfer::Jvm::attach().expect("failed to attach to Jvm");
-    let jsurfer_file = context
-        .load_file(&get_path(CROSSREF_DATA_PATH))
-        .expect("failed to load file via jsurfer");
-    let jsurfer_query = context
-        .compile_query(options.query_string)
-        .expect("failed to compile query via jsurfer");
-    println!("rsonpath/jsurfer Query: {}", options.query_string);
-    println!("JsonSki Query: {}", options.jsonski_query_string);
-    let contents = get_contents(CROSSREF_DATA_PATH);
+fn crossref(c: &mut Criterion, options: BenchmarkOptions<'_>, i: u8, include_jsurf: bool) {
+    let data_path : &str = &format!("crossref/crossref{}.json", i);
+    let contents = get_contents(data_path);
     let query = JsonPathQuery::parse(options.query_string).unwrap();
 
     let mut group = c.benchmark_group(format! {"crossref_{}", options.id});
@@ -57,16 +48,25 @@ fn crossref(c: &mut Criterion, options: BenchmarkOptions<'_>) {
         &contents,
         |b, c| b.iter(|| rsonpath.run::<CountResult>(c)),
     );
-    group.bench_with_input(
-        BenchmarkId::new("jsurfer", options.query_string),
-        &(&jsurfer_file, &jsurfer_query),
-        |b, &(f, q)| {
-            b.iter(|| q.run(f).unwrap());
-        },
+    if include_jsurf{
+        let context = rust_jsurfer::Jvm::attach().expect("failed to attach to Jvm");
+        let jsurfer_file = context
+            .load_file(&get_path(data_path))
+            .expect("failed to load file via jsurfer");
+        let jsurfer_query = context
+            .compile_query(options.query_string)
+            .expect("failed to compile query via jsurfer");
+        group.bench_with_input(
+            BenchmarkId::new("jsurfer", options.query_string),
+            &(&jsurfer_file, &jsurfer_query),
+            |b, &(f, q)| {
+                b.iter(|| q.run(f).unwrap());
+            },
     );
+    }
     if !options.jsonski_query_string.is_empty() {
         let jsonski_query = rust_jsonski::create_jsonski_query(options.jsonski_query_string);
-        let jsonski_record = get_jsonski_record(CROSSREF_DATA_PATH);
+        let jsonski_record = get_jsonski_record(data_path);
         group.bench_with_input(
             BenchmarkId::new("jsonski", options.jsonski_query_string),
             &(&jsonski_record, &jsonski_query),
@@ -87,6 +87,9 @@ pub fn doi(c: &mut Criterion) {
             warm_up_time: Duration::from_secs(10),
             measurement_time: Duration::from_secs(40),
         },
+        8,
+        true,
+
     )
 }
 pub fn title(c: &mut Criterion) {
@@ -94,11 +97,13 @@ pub fn title(c: &mut Criterion) {
         c,
         BenchmarkOptions {
             query_string: "$..title",
-            jsonski_query_string: "$[*].message.items[*].title",
+            jsonski_query_string: "$.items[*].title",
             id: "title",
             warm_up_time: Duration::from_secs(10),
             measurement_time: Duration::from_secs(40),
         },
+        8,
+        true,
     )
 }
 pub fn orcid(c: &mut Criterion) {
@@ -106,11 +111,13 @@ pub fn orcid(c: &mut Criterion) {
         c,
         BenchmarkOptions {
             query_string: "$..author..ORCID",
-            jsonski_query_string: "$[*].message.items[*].author[*].ORCID",
+            jsonski_query_string: "$.items[*].author[*].ORCID",
             id: "orcid",
             warm_up_time: Duration::from_secs(10),
             measurement_time: Duration::from_secs(40),
         },
+        8,
+        true,
     )
 }
 
@@ -119,13 +126,95 @@ pub fn affiliation(c: &mut Criterion) {
         c,
         BenchmarkOptions {
             query_string: "$..affiliation..name",
-            jsonski_query_string: "$[*].message.items[*].author[*].affiliation[*].name",
+            jsonski_query_string: "$.items[*].author[*].affiliation[*].name",
             id: "affiliation",
             warm_up_time: Duration::from_secs(10),
             measurement_time: Duration::from_secs(40),
         },
+        8,
+        true,
     )
 }
 
-criterion_group!(crossref_benches, affiliation, orcid, title, doi);
+pub fn scalability0(c: &mut Criterion){
+    crossref(
+        c,
+        BenchmarkOptions {
+            query_string: "$..affiliation..name",
+            jsonski_query_string: "",
+            id: "scalability0_affiliation",
+            warm_up_time: Duration::from_secs(10),
+            measurement_time: Duration::from_secs(40),
+        },
+        0,
+        false,
+    )
+     
+}
+
+pub fn scalability1(c: &mut Criterion){
+    crossref(
+        c,
+        BenchmarkOptions {
+            query_string: "$..affiliation..name",
+            jsonski_query_string: "",
+            id: "scalability1_affiliation",
+            warm_up_time: Duration::from_secs(10),
+            measurement_time: Duration::from_secs(40),
+        },
+        1,
+        false,
+    )
+     
+}
+
+pub fn scalability2(c: &mut Criterion){
+    crossref(
+        c,
+        BenchmarkOptions {
+            query_string: "$..affiliation..name",
+            jsonski_query_string: "",
+            id: "scalability2_affiliation",
+            warm_up_time: Duration::from_secs(10),
+            measurement_time: Duration::from_secs(40),
+        },
+        2,
+        false,
+    )
+     
+}
+
+pub fn scalability4(c: &mut Criterion){
+    crossref(
+        c,
+        BenchmarkOptions {
+            query_string: "$..affiliation..name",
+            jsonski_query_string: "",
+            id: "scalability4_affiliation",
+            warm_up_time: Duration::from_secs(10),
+            measurement_time: Duration::from_secs(40),
+        },
+        4,
+        false,
+    )
+     
+}
+
+pub fn scalability8(c: &mut Criterion){
+    crossref(
+        c,
+        BenchmarkOptions {
+            query_string: "$..affiliation..name",
+            jsonski_query_string: "",
+            id: "scalability8_affiliation",
+            warm_up_time: Duration::from_secs(10),
+            measurement_time: Duration::from_secs(40),
+        },
+        8,
+        false,
+    )
+     
+}
+
+criterion_group!(crossref_benches, affiliation, orcid, title, doi, scalability0, scalability1, scalability2, scalability4, scalability8);
 criterion_main!(crossref_benches);
