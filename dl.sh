3a9641671767fe9776ce7072184f6b18ace904b3
Mateusz <mat@gienieczko.com>
2022-11-08 00:13:31 +0000

    chore: cleanup

--- dl.sh
+++ dl.sh
@@ -11,6 +11,7 @@ mkdir -p $dirpath/data/twitter
 wget  https://zenodo.org/record/7229287/files/twitter.json -O $dirpath/data/twitter/twitter.json
 
 echo "Downloading crossref.tar.gz"
+mkdir -p $dirpath/data/crossref
 wget  https://zenodo.org/record/7231920/files/crossref.tar.gz -O $dirpath/data/crossref/crossref.tar.gz
 echo "Extracting crossref.tar.gz"
-cd data;tar xvfz crossref.tar.gz;rm crossref.tar.gz
+cd data/crossref; tar xvfz crossref.tar.gz; rm crossref.tar.gz
