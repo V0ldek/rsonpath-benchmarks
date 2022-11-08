1897d62964f263b5a49caf5e17a8c93f96779cd4
Charles Paperman <charles.paperman@univ-lille.fr>
2022-11-08 21:33:39 +0100

    adding openfood

--- dl.sh
+++ dl.sh
@@ -2,6 +2,10 @@ path=$(pwd)/$0
 dirpath=$(dirname $path)
 echo $dirpath
 
+echo "Downloading openfood.json"
+mkdir -p $dirpath/data/openfood
+wget https://zenodo.org/record/7305505/files/openfood.json -O $dirpath/data/openfood/openfood.json
+
 echo "Downloading ast.json"
 mkdir -p $dirpath/data/ast/
 wget https://zenodo.org/record/7229269/files/ast.json -O $dirpath/data/ast/ast.json
