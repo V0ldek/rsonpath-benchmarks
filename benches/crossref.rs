f10c59f6e7308c2c223aa274f6fe962103187b09
V0ldek <mat@gienieczko.com>
2022-11-09 00:49:34 +0000

    Major revamping of benchmark framework and datasets (#2)
    Co-authored-by: Charles Paperman <charles.paperman@univ-lille.fr>

--- benches/crossref.rs
+++ benches/crossref.rs
@@ -1,131 +1,110 @@
-use core::time::Duration;
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use rsonpath::engine::result::CountResult;
-use rsonpath::engine::{Input, Runner};
-use rsonpath::query::JsonPathQuery;
-use rsonpath::stackless::StacklessRunner;
-use rsonpath_benchmarks::rust_jsurfer;
-use rsonpath_benchmarks::rust_jsonski;
+use rsonpath_benchmarks::prelude::*;
 
-use std::fs;
+pub fn doi(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    let benchset = Benchset::new("DOI", dataset::crossref(2))?
+        .add_all_targets_except_jsonski("$..DOI")?
+        .finish();
 
-const ROOT_TEST_DIRECTORY: &str = "./data";
-const CROSSREF_DATA_PATH : &str = "crossref/crossref.json";
-fn get_jsonski_record(test_path: &str) -> rust_jsonski::JsonSkiRecord {
-    let path = format!("{}/{}", ROOT_TEST_DIRECTORY, test_path);
-    rust_jsonski::load_jsonski_record(&path)
-}
-fn get_path(test_path: &str) -> String {
-    format!("{}/{}", ROOT_TEST_DIRECTORY, test_path)
+    benchset.run(c);
+
+    Ok(())
 }
 
-fn get_contents(test_path: &str) -> Input {
-    let raw = fs::read_to_string(get_path(test_path)).unwrap();
-    Input::new(raw)
+pub fn title(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    let benchset = Benchset::new("title", dataset::crossref(2))?
+        .add_all_targets_except_jsonski("$..title")?
+        .add_target(BenchTarget::JsonSki("$.items[*].title"))?
+        .finish();
+
+    benchset.run(c);
+
+    Ok(())
 }
 
-struct BenchmarkOptions<'a> {
-    pub query_string: &'a str,
-    pub jsonski_query_string: &'a str,
-    pub id: &'a str,
-    pub warm_up_time: Duration,
-    pub measurement_time: Duration,
+pub fn orcid(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    let benchset = Benchset::new("orcid", dataset::crossref(2))?
+        .add_all_targets_except_jsonski("$..author..ORCID")?
+        .add_target(BenchTarget::JsonSki(
+            "$.items[*].author[*].ORCID",
+        ))?
+        .finish();
+
+    benchset.run(c);
+
+    Ok(())
 }
 
-fn crossref(c: &mut Criterion, options: BenchmarkOptions<'_>) {
-    let context = rust_jsurfer::Jvm::attach().expect("failed to attach to Jvm");
-    let jsurfer_file = context
-        .load_file(&get_path(CROSSREF_DATA_PATH))
-        .expect("failed to load file via jsurfer");
-    let jsurfer_query = context
-        .compile_query(options.query_string)
-        .expect("failed to compile query via jsurfer");
-    println!("rsonpath/jsurfer Query: {}", options.query_string);
-    println!("JsonSki Query: {}", options.jsonski_query_string);
-    let contents = get_contents(CROSSREF_DATA_PATH);
-    let query = JsonPathQuery::parse(options.query_string).unwrap();
+pub fn author_affiliation(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    let benchset = Benchset::new("author_affiliation", dataset::crossref(2))?
+        .add_all_targets_except_jsonski("$..author..affiliation..name")?
+        .add_target(BenchTarget::JsonSki(
+            "$.items[*].author[*].affiliation[*].name",
+        ))?
+        .finish();
 
-    let mut group = c.benchmark_group(format! {"crossref_{}", options.id});
-    group.warm_up_time(options.warm_up_time);
-    group.measurement_time(options.measurement_time);
-    group.throughput(criterion::Throughput::BytesDecimal(contents.len() as u64));
+    benchset.run(c);
 
-    let rsonpath = StacklessRunner::compile_query(&query);
-
-    group.bench_with_input(
-        BenchmarkId::new("rsonpath", options.query_string),
-        &contents,
-        |b, c| b.iter(|| rsonpath.run::<CountResult>(c)),
-    );
-    group.bench_with_input(
-        BenchmarkId::new("jsurfer", options.query_string),
-        &(&jsurfer_file, &jsurfer_query),
-        |b, &(f, q)| {
-            b.iter(|| q.run(f).unwrap());
-        },
-    );
-    if !options.jsonski_query_string.is_empty() {
-        let jsonski_query = rust_jsonski::create_jsonski_query(options.jsonski_query_string);
-        let jsonski_record = get_jsonski_record(CROSSREF_DATA_PATH);
-        group.bench_with_input(
-            BenchmarkId::new("jsonski", options.jsonski_query_string),
-            &(&jsonski_record, &jsonski_query),
-            |b, &(r, q)| {
-                b.iter(|| rust_jsonski::call_jsonski(q, r));
-            },
-        );
-    }
-    group.finish();
+    Ok(())
 }
-pub fn doi(c: &mut Criterion) {
-    crossref(
-        c,
-        BenchmarkOptions {
-            query_string: "$..DOI",
-            jsonski_query_string: "",
-            id: "DOI",
-            warm_up_time: Duration::from_secs(10),
-            measurement_time: Duration::from_secs(40),
-        },
-    )
-}
-pub fn title(c: &mut Criterion) {
-    crossref(
-        c,
-        BenchmarkOptions {
-            query_string: "$..title",
-            jsonski_query_string: "$[*].message.items[*].title",
-            id: "title",
-            warm_up_time: Duration::from_secs(10),
-            measurement_time: Duration::from_secs(40),
-        },
-    )
-}
-pub fn orcid(c: &mut Criterion) {
-    crossref(
-        c,
-        BenchmarkOptions {
-            query_string: "$..author..ORCID",
-            jsonski_query_string: "$[*].message.items[*].author[*].ORCID",
-            id: "orcid",
-            warm_up_time: Duration::from_secs(10),
-            measurement_time: Duration::from_secs(40),
-        },
-    )
+pub fn editor_affiliation(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    let benchset = Benchset::new("editor", dataset::crossref(2))?
+        .add_all_targets_except_jsonski("$..editor..affiliation..name")?
+        .add_target(BenchTarget::JsonSki(
+            "$.items[*].editor[*].affiliation[*].name",
+        ))?
+        .finish();
+    benchset.run(c);
+    Ok(())
 }
 
-pub fn affiliation(c: &mut Criterion) {
-    crossref(
-        c,
-        BenchmarkOptions {
-            query_string: "$..affiliation..name",
-            jsonski_query_string: "$[*].message.items[*].author[*].affiliation[*].name",
-            id: "affiliation",
-            warm_up_time: Duration::from_secs(10),
-            measurement_time: Duration::from_secs(40),
-        },
-    )
+fn scalability(c: &mut Criterion, size: u32) -> Result<(), BenchmarkError> {
+    let benchset = Benchset::new(
+        format!("scalability_affiliation{size}"),
+        dataset::crossref(size),
+    )?
+    .add_target(BenchTarget::Rsonpath("$..affiliation..name"))?
+    .finish();
+
+    benchset.run(c);
+
+    Ok(())
 }
 
-criterion_group!(crossref_benches, affiliation, orcid, title, doi);
-criterion_main!(crossref_benches);
+pub fn scalability0(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    scalability(c, 0)
+}
+
+pub fn scalability1(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    scalability(c, 1)
+}
+
+pub fn scalability2(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    scalability(c, 2)
+}
+
+pub fn scalability4(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    scalability(c, 4)
+}
+
+pub fn scalability8(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    scalability(c, 8)
+}
+
+pub fn scalability16(c: &mut Criterion) -> Result<(), BenchmarkError> {
+    scalability(c, 16)
+}
+
+benchsets!(
+    crossref_benches,
+    author_affiliation,
+    editor_affiliation,
+    orcid,
+    title,
+    doi,
+    scalability0,
+    scalability1,
+    //scalability2,
+    scalability4,
+    scalability8,
+    scalability16,
+);
--- src/rust_jsurfer.rs
+++ src/rust_jsurfer.rs
@@ -1,8 +1,11 @@
-use eyre::{eyre, Result};
+use std::num::TryFromIntError;
+
+use crate::framework::implementation::Implementation;
 use jni::objects::{JClass, JObject, JValue};
-use jni::signature::{JavaType, Primitive, ReturnType, TypeSignature};
+use jni::signature::{JavaType, Primitive, TypeSignature};
 use jni::{AttachGuard, InitArgsBuilder, JNIEnv, JNIVersion, JavaVM};
 use lazy_static::lazy_static;
+use thiserror::Error;
 
 macro_rules! package {
     () => {
@@ -11,6 +14,7 @@ macro_rules! package {
 }
 
 const SHIM_CLASS: &str = concat!(package!(), "/Shim");
+const QUERY_CLASS: &str = concat!(package!(), "/CompiledQuery");
 const FILE_CLASS: &str = concat!(package!(), "/JsonFile");
 const COMPILE_METHOD: &str = "compileQuery";
 const LOAD_METHOD: &str = "loadFile";
@@ -24,10 +28,14 @@ fn json_file_type() -> JavaType {
     JavaType::Object(FILE_CLASS.to_owned())
 }
 
+fn compiled_query_type() -> JavaType {
+    JavaType::Object(QUERY_CLASS.to_owned())
+}
+
 fn load_file_sig() -> String {
     TypeSignature {
         args: vec![string_type()],
-        ret: ReturnType::Object,
+        ret: json_file_type(),
     }
     .to_string()
 }
@@ -35,7 +43,7 @@ fn load_file_sig() -> String {
 fn compile_query_sig() -> String {
     TypeSignature {
         args: vec![string_type()],
-        ret: ReturnType::Object,
+        ret: compiled_query_type(),
     }
     .to_string()
 }
@@ -43,7 +51,7 @@ fn compile_query_sig() -> String {
 fn overhead_sig() -> String {
     TypeSignature {
         args: vec![],
-        ret: ReturnType::Object,
+        ret: compiled_query_type(),
     }
     .to_string()
 }
@@ -51,7 +59,7 @@ fn overhead_sig() -> String {
 fn run_sig() -> String {
     TypeSignature {
         args: vec![json_file_type()],
-        ret: ReturnType::Primitive(Primitive::Long),
+        ret: JavaType::Primitive(Primitive::Long),
     }
     .to_string()
 }
@@ -68,12 +76,11 @@ pub struct JSurferContext<'j> {
 }
 
 pub struct CompiledQuery<'j> {
-    env: &'j JNIEnv<'j>,
-    query: JObject<'j>,
+    query_object: JObject<'j>,
 }
 
 pub struct LoadedFile<'j> {
-    file: JValue<'j>,
+    file_object: JValue<'j>,
 }
 
 pub struct Overhead<'j> {
@@ -82,8 +89,9 @@ pub struct Overhead<'j> {
 }
 
 impl Jvm {
-    fn new() -> Result<Self> {
-        let jar_path = std::env::var("RSONPATH_BENCH_JSURFER_SHIM_JAR_PATH")?;
+    fn new() -> Result<Self, JSurferError> {
+        let jar_path = std::env::var("RSONPATH_BENCH_JSURFER_SHIM_JAR_PATH")
+            .map_err(JSurferError::NoJarPathEnvVar)?;
 
         let jvm_args = InitArgsBuilder::new()
             .version(JNIVersion::V8)
@@ -96,7 +104,7 @@ impl Jvm {
         Ok(Jvm(jvm))
     }
 
-    pub fn attach() -> Result<JSurferContext<'static>> {
+    pub fn attach() -> Result<JSurferContext<'static>, JSurferError> {
         let guard = JVM.0.attach_current_thread()?;
         let shim = guard.find_class(SHIM_CLASS)?;
 
@@ -105,48 +113,11 @@ impl Jvm {
 }
 
 impl<'j> JSurferContext<'j> {
-    pub fn env(&self) -> &JNIEnv {
+    pub fn env(&self) -> &JNIEnv<'j> {
         &self.jvm
     }
 
-    pub fn compile_query(&'j self, query: &'_ str) -> Result<CompiledQuery<'j>> {
-        let query_string = self.env().new_string(query)?;
-        let compile_query_result = self.env().call_static_method(
-            self.shim,
-            COMPILE_METHOD,
-            compile_query_sig(),
-            &[query_string.into()],
-        )?;
-
-        let compiled_query_object = match compile_query_result {
-            JValue::Object(query_obj) => query_obj,
-            _ => {
-                return Err(eyre!(
-                    "compileQuery returned something other than an object"
-                ))
-            }
-        };
-
-        Ok(CompiledQuery {
-            env: self.env(),
-            query: compiled_query_object,
-        })
-    }
-
-    pub fn load_file(&'j self, file_path: &'_ str) -> Result<LoadedFile<'j>> {
-        let file_string = self.env().new_string(file_path)?;
-
-        let loaded_file = self.env().call_static_method(
-            self.shim,
-            LOAD_METHOD,
-            load_file_sig(),
-            &[file_string.into()],
-        )?;
-
-        Ok(LoadedFile { file: loaded_file })
-    }
-
-    pub fn create_overhead(&'j self) -> Result<Overhead<'j>> {
+    pub fn create_overhead(&'j self) -> Result<Overhead<'j>, JSurferError> {
         let overhead_result =
             self.env()
                 .call_static_method(self.shim, OVERHEAD_METHOD, overhead_sig(), &[])?;
@@ -154,8 +125,10 @@ impl<'j> JSurferContext<'j> {
         let overhead_object = match overhead_result {
             JValue::Object(obj) => obj,
             _ => {
-                return Err(eyre!(
-                    "overheadShim returned something other than an object"
+                return Err(type_error(
+                    OVERHEAD_METHOD,
+                    "Object",
+                    overhead_result.type_name(),
                 ))
             }
         };
@@ -167,28 +140,142 @@ impl<'j> JSurferContext<'j> {
     }
 }
 
-impl<'j> CompiledQuery<'j> {
-    pub fn run(&self, loaded_file: &LoadedFile) -> Result<i64> {
+impl<'j> Overhead<'j> {
+    pub fn run(&self, loaded_file: &LoadedFile) -> Result<i64, JSurferError> {
         let result =
             self.env
-                .call_method(self.query, RUN_METHOD, run_sig(), &[loaded_file.file])?;
+                .call_method(self.shim, RUN_METHOD, run_sig(), &[loaded_file.file_object])?;
 
         match result {
             JValue::Long(res) => Ok(res),
-            _ => Err(eyre!("run returned something else than long")),
+            _ => panic!("run returned something else than long"),
         }
     }
 }
 
-impl<'j> Overhead<'j> {
-    pub fn run(&self, loaded_file: &LoadedFile) -> Result<i64> {
-        let result = self
-            .env
-            .call_method(self.shim, RUN_METHOD, run_sig(), &[loaded_file.file])?;
+pub struct JSurfer {
+    context: JSurferContext<'static>,
+}
+
+impl JSurfer {
+    fn env(&self) -> &JNIEnv<'static> {
+        self.context.env()
+    }
+
+    fn shim(&self) -> JClass<'static> {
+        self.context.shim
+    }
+}
+
+impl Implementation for JSurfer {
+    type Query = CompiledQuery<'static>;
+
+    type File = LoadedFile<'static>;
+
+    type Error = JSurferError;
+
+    fn id() -> &'static str {
+        "jsurfer"
+    }
+
+    fn new() -> Result<Self, Self::Error> {
+        Ok(JSurfer {
+            context: Jvm::attach()?,
+        })
+    }
+
+    fn load_file(&self, path: &str) -> Result<Self::File, Self::Error> {
+        let file_string = self.env().new_string(path)?;
+
+        let loaded_file = self.env().call_static_method(
+            self.shim(),
+            LOAD_METHOD,
+            load_file_sig(),
+            &[file_string.into()],
+        )?;
+
+        Ok(LoadedFile {
+            file_object: loaded_file,
+        })
+    }
+
+    fn compile_query(&self, query: &str) -> Result<Self::Query, Self::Error> {
+        let query_string = self.env().new_string(query)?;
+        let compile_query_result = self.env().call_static_method(
+            self.shim(),
+            COMPILE_METHOD,
+            compile_query_sig(),
+            &[query_string.into()],
+        )?;
+
+        let compiled_query_object = match compile_query_result {
+            JValue::Object(query_obj) => query_obj,
+            _ => {
+                return Err(type_error(
+                    COMPILE_METHOD,
+                    "Object",
+                    compile_query_result.type_name(),
+                ))
+            }
+        };
+
+        Ok(CompiledQuery {
+            query_object: compiled_query_object,
+        })
+    }
+
+    fn run(&self, query: &Self::Query, file: &Self::File) -> Result<u64, Self::Error> {
+        let result = self.env().call_method(
+            query.query_object,
+            RUN_METHOD,
+            run_sig(),
+            &[file.file_object],
+        )?;
 
         match result {
-            JValue::Long(res) => Ok(res),
-            _ => Err(eyre!("run returned something else than long")),
+            JValue::Long(res) => res
+                .try_into()
+                .map_err(|err| JSurferError::ResultOutOfRange {
+                    value: res,
+                    source: err,
+                }),
+            _ => {
+                return Err(type_error(
+                    RUN_METHOD,
+                    "Long (non-negative)",
+                    result.type_name(),
+                ))
+            }
         }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum JSurferError {
+    #[error("could not find JSurfer shim jar path (this should be set by the build script)")]
+    NoJarPathEnvVar(std::env::VarError),
+    #[error("error while setting up the JVM")]
+    JvmError(#[from] jni::JvmError),
+    #[error("runtime error in JSurfer code")]
+    JavaRuntimeError(#[from] jni::errors::Error),
+    #[error("JVM method {method} returned {actual} when {expected} was expected")]
+    JavaTypeError {
+        method: String,
+        expected: String,
+        actual: String,
+    },
+    #[error("received result outside of u64 range: {value}")]
+    ResultOutOfRange {
+        value: i64,
+        #[source]
+        source: TryFromIntError,
+    },
+}
+
+fn type_error(method: &str, expected: &str, actual: &str) -> JSurferError {
+    JSurferError::JavaTypeError {
+        method: method.to_owned(),
+        expected: expected.to_owned(),
+        actual: actual.to_owned(),
     }
 }
