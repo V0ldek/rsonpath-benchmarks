7fe67e4e8eeb9610259346123a5c69940c1216fc
cha <charles.paperman@univ-lille.fr>
2023-02-27 20:20:41 +0100

    charts update

--- charts/draw_plot.py
+++ charts/draw_plot.py
@@ -37,3 +37,49 @@ def print_plot(rsonpath, jsurfer, jsonski, exp_label, fileout="plot.png"):
     plot.subplots_adjust(wspace=0.2, left=0.06)
     plot.savefig("plot.png")
     
+def plot_from_dataframe(df, keys=None, width=0.8, colors=dict(simdpath="tab:blue", jsonski="tab:red", rewritten="tab:green")):
+    keys = sorted(df) if not keys else keys
+    plot.rcParams.update({
+    "font.size": 28,
+    "axes.facecolor": "whitesmoke",
+    "font.family": "serif",
+    "figure.figsize":(20, 5)
+    })
+
+    pos = np.array(range(len(df.index)))
+    fig, ax = plot.subplots()
+    fig.set_size_inches((12, 7))
+    ax.grid(color = 'white', linestyle = '-', linewidth = 3, zorder=1)
+    ax.set_xticks(pos)
+    ax.set_xticklabels(df.index)
+    ax.set_ylabel("GB/s")
+    if len(keys) == 1:
+        ax.bar(pos, df[keys[0]], width=width, zorder=4, label=keys[0], color=colors[keys[0]])
+    else: 
+        w = width/len(keys)
+        for i, k in enumerate(keys):
+            npos = pos + (len(keys)-1)*w*((i/(len(keys)-1))-0.5)
+            ax.bar(npos, df[k], width=w, zorder=4, label=k, color=colors[k])
+    ax.legend()
+    fig.tight_layout()
+    return fig
+
+def generate_graphs(path, outpath):
+    import charts.extract_info as ei
+    df = ei.exp_to_dataframe(path).set_index("id")[["rsonpath", "jsonski"]].rename(dict(rsonpath="simdpath"), axis=1)
+    df.to_csv(outpath+"/data.csv")
+
+    df1 = df.filter(items=ei.jsonski_vs_rsonpath, axis=0).drop("N1")
+    fig = plot_from_dataframe(df1)
+    fig.savefig(outpath+"/simdpath_vs_jsonski.png")
+
+    query_orig = list(map(lambda e:e[:-1], ei.query_rewritten))
+    df2 = df.filter(items=query_orig, axis=0)
+    df3 = df.filter(items=ei.query_rewritten, axis=0)[["simdpath"]]
+    df2["rewritten"] = df3.rename(lambda e:e[:-1])
+    fig = plot_from_dataframe(df2)
+    fig.savefig(outpath+"/query_rewritten.png")
+
+    df4 = df.filter(items=ei.query_interest, axis=0)[["simdpath"]] 
+    fig = plot_from_dataframe(df4)
+    fig.savefig(outpath+"/query_interest.png")
--- charts/extract_info.py
+++ charts/extract_info.py
@@ -64,7 +64,7 @@ def format_bench(name):
     a,b = name.split(".json_", maxsplit=1)
     bench = a.split("/")[-1]
     query = b
-    return bench, query, name 
+    return bench.strip(), query.strip(), name.strip()
 
 def process_exp_data(data):
     d2 = {}
@@ -72,8 +72,197 @@ def process_exp_data(data):
         d2[e] = h = {}
         for x in v:
             t = v[x]["throughput"]
+            if not t:
+                continue
             size = t.get("BytesDecimal", t.get("Bytes"))
             stdev = v[x]["estimates"]["median"][1]
             median = v[x]["estimates"]["median"][0]
             h[x] = size/median #(size/(median+stdev), size/median, size/(median-stdev))
     return d2
+
+
+def exp_to_list(path:str):
+    data = get_exp_data(path)
+    processed = process_exp_data(data)
+    L = []
+    for e, v in processed.items():
+        if e[0] != ".":
+            continue
+        t = format_bench(e)
+        x, y, z = v["rsonpath"], v.get("jsonski"), v.get("jsurfer")
+        qid = id_map[t[1]]
+        query = id_queries[qid]     
+        L.append((qid, t[1], t[0], query, x, y, z))
+
+    L.sort(key=lambda e:e[:2])
+    return L
+
+header = ["id", "rsonpath_id", "dataset", "query", "rsonpath", "jsonski", "jsurfer"]
+
+id_map = {
+    "decl_name" : "A1",
+    "included_from" : "A2",
+    "nested_inner" : "A3",
+    "BB1_products_category" : "B1",
+    "BB1'_products_category" : "B1r",
+    "BB2_products_video" : "B2",
+    "BB2'_products_video" : "B2r",
+    "BB3_products_video_only" : "B3",
+    "BB3'_products_video_only" : "B3r",
+    "scalability_affiliation0" : "S0",
+    "scalability_affiliation1" : "S1",
+    "author_affiliation" : "C3",
+    "author_affiliation_descendant" : "C3r",
+    "DOI" : "C1",
+    "editor" : "C4",
+    "editor_descendant" : "C4r",
+    "orcid" : "C5",
+    "orcid_descendant" : "C5r",
+    "scalability_affiliation2" : "S2",
+    "title" : "C2",
+    "title_descendant" : "C2r",
+    "scalability_affiliation4" : "S4",
+    "GMD1_routes" : "G1",
+    "GMD2_travel_modes" : "G2",
+    "GMD2'_travel_modes" : "G2r",
+    "NSPL1_meta_columns" : "N1",
+    "NSPL2_data" : "N2",
+    "added_counties_tags" : "O2",
+    "added_countries_tags_descendant" : "O2r",
+    "specific_ingredients" : "O3",
+    "specific_ingredients_descendant" : "O3r",
+    "vitamins_tags" : "O1",
+    "vitamins_tags_descendant" : "O1r",
+    "all_hashtags" : "Ts1",
+    "hashtags_of_retweets" : "Ts2",
+    "metadata_1" : "Ts3",
+    "metadata_2" : "Ts4",
+    "metadata_3" : "Ts5",
+    "TT1_entities_urls" : "T1",
+    "TT2_text" : "T2",
+    "WM1_items_price" : "Wa1",
+    "WM1'_items_price" : "Wa1r",
+    "WM2_items_name" : "Wa2",
+    "WM2'_items_name" : "Wa2r",
+    "WP1_claims_p150" : "Wi",
+    "WP1'_claims_p150" : "Wir"
+}
+
+id_queries = {
+    "A1": "$..decl.name",
+    "A2": "$..loc.includedFrom.file",
+    "A3": "$..inner..inner..type.qualType",
+    "B1": "$.products[*].categoryPath[*].id",
+    "B1r": "$..categoryPath..id",
+    "B2": "$.products[*].videoChapters[*].chapter",
+    "B2r": "$..videoChapters..chapter",
+    "B3": "$.products[*].videoChapters",
+    "B3r": "$..videoChapters",
+    "S0": "$..affiliation..name",
+    "S1": "$..affiliation..name",
+    "C3": "$.items[*].author[*].affiliation[*].name",
+    "C3r": "$..author..affiliation..name",
+    "C1": "$..DOI",
+    "C4": "$.items[*].editor[*].affiliation[*].name",
+    "C4r": "$..editor..affiliation..name",
+    "C5": "$.items[*].author[*].ORCID",
+    "C5r": "$..author..ORCID",
+    "S2": "$..affiliation..name",
+    "C2": "$.items[*].title",
+    "C2r": "$..title",
+    "S4": "$..affiliation..name",
+    "G1": "$[*].routes[*].legs[*].steps[*].distance.text",
+    "G2": "$[*].available_travel_modes",
+    "G2r": "$..available_travel_modes",
+    "N1": "$.meta.view.columns[*].name",
+    "N2": "$.data[*][*][*]",
+    "O2": "$.products[*].added_countries_tags",
+    "O2r": "$..added_countries_tags",
+    "O3": "$.products[*].specific_ingredients[*].ingredient",
+    "O3r": "$..specific_ingredients..ingredient",
+    "O1": "$.products[*].vitamins_tags",
+    "O1r": "$..vitamins_tags",
+    "Ts1": "$..hashtags..text",
+    "Ts2": "$..retweeted_status..hashtags..text",
+    "Ts3": "$.search_metadata.count",
+    "Ts4": "$..search_metadata.count",
+    "Ts5": "$..count",
+    "T1": "$[*].entities.urls[*].url",
+    "T2": "$[*].text",
+    "Wa1": "$.items[*].bestMarketplacePrice.price",
+    "Wa1r": "$..bestMarketplacePrice.price",
+    "Wa2": "$.items[*].name",
+    "Wa2r": "$..name",
+    "Wi": "$[*].claims.P150[*].mainsnak.property",
+    "Wir": "$..P150..mainsnak.property"
+}
+
+jsonski_vs_rsonpath = [
+    "B1",
+    "B2",
+    "B3",
+    "G1",
+    "G2",
+    "N1",
+    "N2",
+    "T1",
+    "T2",
+    "Wa1",
+    "Wa2",
+    "Wi"
+]
+
+query_rewritten = [
+    "B1r",
+    "B2r",
+    "B3r",
+    "G2r",
+    "Wa1r",
+    "Wa2r",
+    "Wir"
+]
+
+query_interest = [
+    "A1",
+    "A3",
+    "C1",
+    "C3",
+    "C3r",
+    "C4",
+    "C4r",
+    "C5",
+    "Ts3",
+    "Ts4",
+    "Ts5"
+]
+
+def get_table():
+    import texttable
+    T=texttable.Texttable(max_width=0)
+    T.header(header)
+    T.set_chars([' ', '|', '|', '-'])
+    T.set_deco(texttable.Texttable.VLINES|texttable.Texttable.HEADER|texttable.Texttable.BORDER)
+    return T
+
+
+def print_table_csv(path:str):
+    import csv, sys
+
+    L = exp_to_list(path)
+    writer = csv.writer(sys.stdout)
+    writer.writerow(header)
+    writer.writerows(L)
+
+def table_markdown(path:str):
+    L = exp_to_list(path)
+    T = get_table()
+    for e in L:
+        T.add_row(e)
+    return "\n".join(T.draw().split("\n")[0:-1])
+
+def exp_to_dataframe(path:str):
+    L = exp_to_list(path)
+    import pandas
+    DF = pandas.DataFrame(L, columns=header)
+    return DF
+
