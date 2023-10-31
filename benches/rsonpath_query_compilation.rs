f7766735e181be6f2dda111f51169dc8ee20db2b
Mateusz Gienieczko <mat@gienieczko.com>
2023-10-31 01:09:10 +0000

    Move types as-is to a separate crate.

--- benches/rsonpath_query_compilation.rs
+++ benches/rsonpath_query_compilation.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use rsonpath::engine::{Compiler, RsonpathEngine};
-use rsonpath::query::JsonPathQuery;
+use rsonpath_syntax::JsonPathQuery;
 
 fn rsonpath_query_compilation(c: &mut Criterion, query_string: &str) {
     let mut group = c.benchmark_group(format! {"rsonpath_{query_string}"});
